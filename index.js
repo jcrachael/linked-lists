a21b3d6297de644a2fb5535c41db6ea5c563fa70
Rachael Cole <jcrachael@gmail.com>
2022-11-05 13:20:36 +1100

    Feature: LinkedList class methods finished

--- index.js
+++ index.js
@@ -7,32 +7,37 @@ class Node {
 
 // This will represent the full list
 class LinkedList {
-    constructor(headNode) {
-        this.HEAD = headNode;
+    constructor() {
+        this.HEAD = null;
         this.HEAD.nextNode = null;
     }
-
     // Add a Node to the end of the LinkedList
     append(value) {
         // Make a Node with this value
         const newNode = new Node(value);
+        if (this.HEAD === null) {
+            return (this.HEAD = newNode);
+        } 
         // store the current head node in a temp variable
         let temp = this.HEAD;
         // if the head node has a nextNode, change the temp
         // variable to store the head's nextNode and repeat until
         // you reach a node with no nextNode (i.e., the tail node)
         while (temp.nextNode != null) {
-            temp = temp.nextNode;
+           temp = temp.nextNode;
         }
         // set the (previous) tail node to point to the new Node
         temp.nextNode = newNode;
-        // Append that Node to the end of the LinkedList
-    };
-
+    }
+        
+    
     // Add a Node to the start of the LinkedList
     prepend(value) {
         // Make a new Node with this value
         let newHeadNode = new Node(value);
+        if (this.HEAD === null) {
+            return (this.HEAD = newHeadNode)
+        }
         // Set the newHeadNode to point to this.HEAD (the old head)
         newHeadNode.nextNode = this.HEAD;
         // set this.HEAD to be newHeadNode
@@ -41,6 +46,9 @@ class LinkedList {
 
     // Return the total number of Nodes in the LinkedList
     size() {
+        if (this.HEAD === null) {
+            return null;
+        }
         let temp = this.HEAD;
         let count = 0;
         while (temp != null) {
@@ -52,19 +60,28 @@ class LinkedList {
 
     // Return the first Node in the LinkedList
     head() {
-        return this.HEAD.value;
+        if (this.HEAD === null) {
+            return null;
+        }
+        return this.HEAD;
     }
 
     // Return the last Node in the LinkedList
     tail() {
+        if (this.HEAD === null) {
+            return null;
+        }
         let temp = this.HEAD;
         while (temp.nextNode != null) {
             temp = temp.nextNode
         }
-        return temp.value;
+        return temp;
     }
 
     at(index) {
+        if (this.HEAD === null) {
+            return null;
+        }
         let temp = this.HEAD;
         let count = 1;
         // while there are still nodes
@@ -77,10 +94,16 @@ class LinkedList {
             count++;
         }
         // once the counter equals the index we want, return that node's value
-        return temp.value;
+        return temp;
     }
 
     pop() {
+        if (this.HEAD === null) {
+            return null;
+        }
+        if (this.HEAD.nextNode === null) {
+            this.HEAD = null;
+        }
         let temp = this.HEAD;
         while (temp.nextNode.nextNode != null) {
             temp = temp.nextNode;
@@ -89,21 +112,91 @@ class LinkedList {
     }
 
     contains(value) {
-        if (true) {
-            return true;
-        } else {
-            return false;
+        if (this.HEAD === null) {
+            return null;
         }
+        let temp = this.HEAD;
+        while (temp.nextNode != null) {
+            if (temp.value === value) {
+                return true
+            }
+            temp = temp.nextNode;
+        }
+        if (temp.value === value) {
+            return true
+        }
+        return false
     }
 
     find(value) {
+        if (this.HEAD === null) {
+            return null;
+        }
         // returns the index of the node containing the value
         // returns null if the value is not found
+        let temp = this.HEAD;
+        let count = 0;
+        while (temp.nextNode != null) {
+            if (temp.value === value) {
+                return count;
+            }
+            temp = temp.nextNode;
+            count++;
+        }
+        if (temp.value === value) {
+            return true
+        }
+        return null;
     }
 
     toString() {
+        if (this.HEAD === null) {
+            return null;
+        }
         // represent the Nodes in this LinkedList as a string, e.g.
         // ( value ) -> ( value ) -> ( value ) -> null
+        let temp = this.HEAD;
+        let result = '';
+        while (temp.nextNode != null) {
+            result = result.concat(`( ${temp.value} ) --> `);
+            temp = temp.nextNode;
+        }
+
+        result = result.concat(`( ${temp.value} ) --> null`);
+        console.log(result);
+        return result;
+    }
+
+    insertAt(value, index) {
+        if (this.HEAD === null) {
+            return null;
+        }
+        // Make the new node
+        const newNode = new Node(value);
+        // Get the node that points to the index
+        const oldBeforeIndex = this.at(index - 1);
+        // Get the node at the index
+        const oldAtIndex = this.at(index);
+        // Make the oldBeforeNode.nextNode point to newNode and
+        // make the newNode.nextNode point to the OldAtNode
+        oldBeforeIndex.nextNode = newNode;
+        newNode.nextNode = oldAtIndex;
+    }
+
+    removeAt(index) {
+        if (this.HEAD === null) {
+            return null;
+        }
+        let length = this.size();
+        if (index > length || index < 0) {
+            return;
+        }
+        if (index === 0) {
+            this.HEAD = HEAD.nextNode;
+        } else {
+            const prevNode = this.at(index - 1);
+            prevNode.nextNode = prevNode.nextNode.nextNode;
+        }
     }
 };
 
